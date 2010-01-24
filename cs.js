460c4232127f2bd9a9c6f16e6d3703226ab44949
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2010-01-24 07:30:42 +0000

    Added stuff to the content script and added four possible icons (in svg form)
    git-svn-id: http://cnx.googlecode.com/svn/trunk@141 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- cs.js
+++ cs.js
@@ -1,12 +1,28 @@
 var cnx = {
 	id: $("a[title='View your nation']").attr("href").match(/\d+$/)[0],
-	te: /^tournament/.test(window.location.hostname),
+	te: window.location.hostname === "tournament.cybernations.net",
 	
 	scrape: function(){
 		
 	}
 };
 
-if (window.location.pathname.toLowerCase() === "/nation_drill_display.asp" && window.location.search.toLowerCase() === "?nation_id=" + cnx.id) {
-	cnx.scrape();
+switch (window.location.pathname.toLowerCase()) {
+	case "/nation_drill_display.asp": {
+		if (window.location.search.toLowerCase() === "?nation_id=" + cnx.id) {
+			chrome.extension.sendRequest(["get", "nation data stale", cnx.te], function(yes){
+				yes && cnx.scrape();
+			});
+		}
+		break;
+	}
+	case "/improvements_purchase.asp":
+	case "/national_wonders_purchase.asp": {
+		break;
+	}
+	case "/technology_purchase.asp":
+	case "/infrastructurebuysell.asp":
+	case "/militarybuysell.asp": {
+		chrome.extension.sendRequest(["set", "nation data stale", te]);
+	}
 }
