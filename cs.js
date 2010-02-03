ef1bc6efed05c2fd891e6fb5ea82ea7555664b2c
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2010-02-03 20:03:26 +0000

    Updated jquery, added icons (these may be changed, but they're needed so we can load the extension), wrote the background page, added rows.json, and added a bunch of stuff to the content script.
    git-svn-id: http://cnx.googlecode.com/svn/trunk@145 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- cs.js
+++ cs.js
@@ -1,17 +1,34 @@
 var cnx = {
 	id: $("a[title='View your nation']").attr("href").match(/\d+$/)[0],
-	te: window.location.hostname === "tournament.cybernations.net",
+	isTE: window.location.hostname === "tournament.cybernations.net",
+	isExtended: /extended=1/i.test(window.location.search),
 	
 	scrape: function(){
+		$(".shadetabs + table > tbody > tr").each(function(){
+			var key = $(this).children(":eq(0)").text().trim(), val = $(this).children(":eq(1)").text().trim();
+		});
 		
+		var data = {
+			date: Date.now(),
+			isStale: false,
+			
+		};
+		
+		chrome.extension.sendRequest({ get: "rows", v: "rows" }, function(rows){
+			for (var k in rows) {
+				console.log(k);
+			}
+		});
 	}
 };
 
 switch (window.location.pathname.toLowerCase()) {
 	case "/nation_drill_display.asp": {
-		if (window.location.search.toLowerCase() === "?nation_id=" + cnx.id) {
-			chrome.extension.sendRequest(["get", "nation data stale", cnx.te], function(yes){
-				yes && cnx.scrape();
+		if ((new RegExp("nation_id=" + cnx.id, "i")).test(window.location.search)) {
+			chrome.extension.sendRequest({ get: "nation_data", v: cnx.isTE ? "te" : "se" }, function(data){
+				if (data.isStale || ((Date.now() - data.date) / (1000 * 60 * 60)) > 24) {
+					cnx.scrape();
+				}
 			});
 		}
 		break;
@@ -23,6 +40,7 @@ switch (window.location.pathname.toLowerCase()) {
 	case "/technology_purchase.asp":
 	case "/infrastructurebuysell.asp":
 	case "/militarybuysell.asp": {
-		chrome.extension.sendRequest(["set", "nation data stale", te]);
+		chrome.extension.sendRequest({ set: "nation_data.isStale", val: true, v: cnx.isTE ? "te" : "se" });
+		break;
 	}
 }
