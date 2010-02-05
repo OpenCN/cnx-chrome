e6c633780bcc7d5f84d5b3e3a6304829f1dbc96f
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2010-02-05 22:21:26 +0000

    Everything is now correctly parsed and saved in cnx-chrome except for the gov type, land, and modifiers. Yay!
    git-svn-id: http://cnx.googlecode.com/svn/trunk@147 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- cs.js
+++ cs.js
@@ -1,14 +1,18 @@
 var cnx = {
 	id: $("a[title='View your nation']").attr("href").match(/\d+$/)[0],
 	isTE: window.location.hostname === "tournament.cybernations.net",
-	isExtended: /extended=1/i.test(window.location.search),
-	
-	traverse: function(cb){
+	isExtended: /extended=1/i.test(window.location.search)
+};
+
+switch (window.location.pathname.toLowerCase()) {
+	case "/nation_drill_display.asp": {
+		if (!(new RegExp("nation_id=" + cnx.id, "i")).test(window.location.search)) { break; }
+		
 		chrome.extension.sendRequest({ get: "rows" }, function(rows){
 			var table = {}, $tr = $(".shadetabs + table > tbody > tr"), trp = 0;
 			
 			$.each(rows, function(id, r){
-				var $td = $tr.eq(trp).children(), k = $td.eq(0).text(), v = $td.eq(1).text().trim();
+				var $td = $tr.eq(trp).children(), k = $td.eq(0).text(), v = $td.eq(1).text().trim() || ($td.eq(1).html() || "").trim() || $td.eq(0).text();
 				
 				if (r.is === k.replace(/\s/g, "") || k.indexOf(r.has) !== -1) {
 					table[id] = v;
@@ -18,38 +22,32 @@ var cnx = {
 				}
 			});
 			
+			var data = {
+				date: Date.now(),
+				isStale: false,
+				id: cnx.id,
+				ruler: table.ruler,
+				name: table.name,
+				modifiers: null,
+				gov: null,
+				land: null
+			};
+			"citizens citizen_tax tax environment infra tech strength global_radiation num_soldiers happiness nukes".split(" ").forEach(function(v){
+				data[v] = parseFloat(table[v].match(/-?[\d,]+(?:\.\d+)?/)[0].replace(",", ""));
+			});
+			chrome.extension.sendRequest({ set: "nation_data", val: data, v: cnx.isTE ? "te" : "se" });
+			
 			cnx.table = table;
 		});
-	},
-	scrape: function(){
-		var t = cnx.table, data = {
-			date: Date.now(),
-			isStale: false,
-			id: cnx.id,
-			ruler: t.ruler,
-			name: t.name
-		};
-		chrome.extension.sendRequest({ set: "nation_data", val: data, v: cnx.isTE ? "te" : "se" });
-	}
-};
-
-switch (window.location.pathname.toLowerCase()) {
-	case "/nation_drill_display.asp": {
-		if ((new RegExp("nation_id=" + cnx.id, "i")).test(window.location.search)) {
-			cnx.traverse(function(){
-				chrome.extension.sendRequest({ get: "nation_data", v: cnx.isTE ? "te" : "se" }, function(data){
-					if (data.isStale || ((Date.now() - data.date) / (1000 * 60 * 60)) > 24) {
-						cnx.scrape();
-					}
-				});
-			});
-		}
+		
 		break;
 	}
+	
 	case "/improvements_purchase.asp":
 	case "/national_wonders_purchase.asp": {
 		break;
 	}
+	
 	case "/technology_purchase.asp":
 	case "/infrastructurebuysell.asp":
 	case "/militarybuysell.asp": {
