70e5297e709338e32e4941af7739ab1fa994ce8f
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2010-02-05 06:09:08 +0000

    Yay, it now gets all the table data correctly!
    git-svn-id: http://cnx.googlecode.com/svn/trunk@146 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- cs.js
+++ cs.js
@@ -3,32 +3,45 @@ var cnx = {
 	isTE: window.location.hostname === "tournament.cybernations.net",
 	isExtended: /extended=1/i.test(window.location.search),
 	
-	scrape: function(){
-		$(".shadetabs + table > tbody > tr").each(function(){
-			var key = $(this).children(":eq(0)").text().trim(), val = $(this).children(":eq(1)").text().trim();
+	traverse: function(cb){
+		chrome.extension.sendRequest({ get: "rows" }, function(rows){
+			var table = {}, $tr = $(".shadetabs + table > tbody > tr"), trp = 0;
+			
+			$.each(rows, function(id, r){
+				var $td = $tr.eq(trp).children(), k = $td.eq(0).text(), v = $td.eq(1).text().trim();
+				
+				if (r.is === k.replace(/\s/g, "") || k.indexOf(r.has) !== -1) {
+					table[id] = v;
+					trp++;
+				} else {
+					return true;
+				}
+			});
+			
+			cnx.table = table;
 		});
-		
-		var data = {
+	},
+	scrape: function(){
+		var t = cnx.table, data = {
 			date: Date.now(),
 			isStale: false,
-			
+			id: cnx.id,
+			ruler: t.ruler,
+			name: t.name
 		};
-		
-		chrome.extension.sendRequest({ get: "rows", v: "rows" }, function(rows){
-			for (var k in rows) {
-				console.log(k);
-			}
-		});
+		chrome.extension.sendRequest({ set: "nation_data", val: data, v: cnx.isTE ? "te" : "se" });
 	}
 };
 
 switch (window.location.pathname.toLowerCase()) {
 	case "/nation_drill_display.asp": {
 		if ((new RegExp("nation_id=" + cnx.id, "i")).test(window.location.search)) {
-			chrome.extension.sendRequest({ get: "nation_data", v: cnx.isTE ? "te" : "se" }, function(data){
-				if (data.isStale || ((Date.now() - data.date) / (1000 * 60 * 60)) > 24) {
-					cnx.scrape();
-				}
+			cnx.traverse(function(){
+				chrome.extension.sendRequest({ get: "nation_data", v: cnx.isTE ? "te" : "se" }, function(data){
+					if (data.isStale || ((Date.now() - data.date) / (1000 * 60 * 60)) > 24) {
+						cnx.scrape();
+					}
+				});
 			});
 		}
 		break;
