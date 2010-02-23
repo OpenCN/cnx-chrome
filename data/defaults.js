48a1ec0d2d3c9195ba53299688435ddeeb283963
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2010-02-23 01:06:20 +0000

    Updated jquery to 1.4.2 and moved the default stuff to defaults.js.
    git-svn-id: http://cnx.googlecode.com/svn/trunk@151 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

---
+++ data/defaults.js
@@ -0,0 +1,12 @@
+(function(localStorage){
+
+var v = localStorage.v;
+if (!v) {
+	localStorage.v = 1;
+	localStorage.se = localStorage.te = '{"nation_data":{"date":0, "isStale":true}}';
+	
+}
+
+if (v <= 1) {}
+
+})(localStorage);
