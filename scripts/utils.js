36884989819539ba9c9b8c9f93431395daa6591f
mindstormskd@gmail.com <mindstormskd@gmail.com@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2012-02-17 08:02:34 +0000

    bye-bye scripts
    git-svn-id: http://cnx.googlecode.com/svn/trunk@171 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- scripts/utils.js
+++
@@ -1,8 +0,0 @@
-var utils = {
-	getJSON: function(path){
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", chrome.extension.getURL(path), false);
-		xhr.send();
-		return JSON.parse(xhr.responseText);
-	}
-};
